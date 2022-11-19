4c54a78dae700e0acecf0cf768549fd5467ca6fa
SaidqodirxonUz <115058529+Saidqodirxon@users.noreply.github.com>
2022-11-19 13:40:10 +0500

    initial commit

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
-let answer = prompt("sizda nechi pul bor?");
-
+// barcha pullar
+let answer = prompt("Sizda nechi pul bor?");
+//Tasodifiy kun
+let kun = Math.floor(Math.random() * (30 - 3 + 3));
+console.log(kun);
 // console.log(answer);
 
-
-let dollar = 9433.34;
-let yeuro = 10354.03; 
+//valyuta kurs
+let dollar = 11250;
+let yeuro = 12864;
 
 // harajatlar:
 
-let borish_kelish = dollar * 500;
-let mex_tolov = dollar * 250;
-let kongil_ochar = yeuro * 120;
+let borish_kelish = dollar * 350;
+let mex_tolov = dollar * 680;
+let kongil_ochar = yeuro * 230;
 
 // jami:
 
 const jami = borish_kelish + mex_tolov + kongil_ochar;
 
-if(answer >= jami){
-    document.querySelector("h1").textContent = "Oq Yo'l";
-}else{
-    document.querySelector("h1").textContent = "sizda pul yetarlicha emas!";
-}
+// Max = 30
+// Min = 3
 
+if (answer >= jami) {
+  document.querySelector("h1").textContent = "Oq Yo'l";
+} else {
+  document.querySelector(
+    "h1"
+  ).textContent = `Sizda pul yetarlicha emas ${kun} kundan keyin urinib ko'ring`;
+}
